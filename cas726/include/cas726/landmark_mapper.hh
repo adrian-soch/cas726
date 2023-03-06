66cbfeb1a7d1a23e8fc1dbae879a59c80bc69725
adrian-soch <adr.soch@outlook.com>
2023-03-05 21:51:55 -0500

    Add landmark viz and helper

--- cas726/include/cas726/landmark_mapper.hh
+++ cas726/include/cas726/landmark_mapper.hh
@@ -2,15 +2,18 @@
 #define CAS726_LANDMARK_MAPPER_HH
 
 #include "rclcpp/rclcpp.hpp"
+
 #include <chrono>
-#include <condition_variable>
 #include <thread>
+#include <condition_variable>
 
 //Message types
-#include "cas726_interfaces/srv/detect_objects.hpp"
 #include "cas726_interfaces/msg/bounding_box.hpp"
+#include "cas726_interfaces/srv/detect_objects.hpp"
+
 #include "sensor_msgs/msg/image.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
+#include <visualization_msgs/msg/marker.hpp>
 
 //For message filters
 #include <message_filters/subscriber.h>
@@ -26,117 +29,137 @@
 
 namespace cas726 {
 
-  /**
-   * Landmark Mapper is a class that creates a map of landmarks. Each landmark is 
-   * detected in the RGB images as an object of interest using a service call to a
-   * python node running a standard model. Landmarks are then tracked with an EKF.
-   */
-  class LandmarkMapper : public rclcpp::Node {
+// Helper Class
+class Landmarks {
     public:
-      //here implement constructor worker_thread_(&LandmarkMapper::run,this), 
-      LandmarkMapper() : Node ("cas726_landmark_mapper"), quit_(false) {
-	//first declare parameters
-	image_topic_ = this->declare_parameter("image_topic","/front/color_image");
-	depth_cloud_topic_ = this->declare_parameter("depth_cloud_topic","/front/points");
 
-	detect_srv_topic_ = this->declare_parameter("detect_srv_topic","/object_detector/detect");
-	odom_frame_ = this->declare_parameter("odom_frame","odom");
-	map_frame_ = this->declare_parameter("map_frame","map");
-	base_frame_ = this->declare_parameter("base_frame","base_link");
+        void append(const std::string &label, 
+            const geometry_msgs::msg::Point &point) {
+            labels.push_back(label);
+            points.push_back(point);
+        }
+        
+        void clear() {
+            labels.clear();
+            points.clear();
+        }
 
-	last_pose_ = Eigen::Affine3d::Identity();
-	//define the quality of service profiles
-	rclcpp::QoS qos(10);
-	auto rmw_qos_profile = qos.get_rmw_qos_profile();
-	
-	//next set up subscriber filters
-	image_subscriber_.subscribe(this, image_topic_, rmw_qos_profile);
-	depth_cloud_subscriber_.subscribe(this, depth_cloud_topic_, rmw_qos_profile);
+        // Struct of Vectors used so we can
+        //  pass the points to other functions
+        std::vector<std::string> labels;
+        std::vector<geometry_msgs::msg::Point> points;
+};
 
-	//synchronized callback
-	image_sync_ = std::make_shared<message_filters::TimeSynchronizer<sensor_msgs::msg::Image, 
-		              sensor_msgs::msg::PointCloud2>>(image_subscriber_, depth_cloud_subscriber_, 1);
+/**
+ * Landmark Mapper is a class that creates a map of landmarks. Each landmark is 
+ * detected in the RGB images as an object of interest using a service call to a
+ * python node running a standard model. Landmarks are then tracked with an EKF.
+ */
+class LandmarkMapper : public rclcpp::Node {
+    public:
+    //here implement constructor worker_thread_(&LandmarkMapper::run,this), 
+    LandmarkMapper() : Node ("cas726_landmark_mapper"), quit_(false) {
+        //first declare parameters
+        image_topic_ = this->declare_parameter("image_topic","/front/color_image");
+        depth_cloud_topic_ = this->declare_parameter("depth_cloud_topic","/front/points");
+
+        detect_srv_topic_ = this->declare_parameter("detect_srv_topic","/object_detector/detect");
+        odom_frame_ = this->declare_parameter("odom_frame","odom");
+        map_frame_ = this->declare_parameter("map_frame","map");
+        base_frame_ = this->declare_parameter("base_frame","base_link");
+
+        last_pose_ = Eigen::Affine3d::Identity();
+        //define the quality of service profiles
+        rclcpp::QoS qos(10);
+        auto rmw_qos_profile = qos.get_rmw_qos_profile();
+        
+        //next set up subscriber filters
+        image_subscriber_.subscribe(this, image_topic_, rmw_qos_profile);
+        depth_cloud_subscriber_.subscribe(this, depth_cloud_topic_, rmw_qos_profile);
+
+        //synchronized callback
+        image_sync_ = std::make_shared<message_filters::TimeSynchronizer<sensor_msgs::msg::Image, 
+                        sensor_msgs::msg::PointCloud2>>(image_subscriber_, depth_cloud_subscriber_, 1);
 
         image_sync_->registerCallback(std::bind(&LandmarkMapper::image_callback, this, std::placeholders::_1, 
-				std::placeholders::_2));
+            std::placeholders::_2));
 
-	//tf listner 
-	tf_buffer_ =
-		std::make_unique<tf2_ros::Buffer>(this->get_clock());
-	tf_listener_ =
-		std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+        //tf listner 
+        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-	//set up automatic timer for update publish 1 per second
-	std::chrono::milliseconds update_duration(1000);
+        //set up automatic timer for update publish 1 per second
+        std::chrono::milliseconds update_duration(1000);
         timer_ = this->create_wall_timer(update_duration, 
-		 std::bind(&LandmarkMapper::update_callback, this));	
+        std::bind(&LandmarkMapper::update_callback, this));	
 
-	//TODO: set up marker Viz publisher
-  
-	//Client to detect objects
-	detect_client_ = this->create_client<cas726_interfaces::srv::DetectObjects>(detect_srv_topic_);
+        // set up marker Viz publisher
+        marker_publisher_ = this->create_publisher<visualization_msgs::msg::Marker>("markers", 1);
+        
+        //Client to detect objects
+        detect_client_ = this->create_client<cas726_interfaces::srv::DetectObjects>(detect_srv_topic_);
 
-      }
-      virtual ~LandmarkMapper() {
-      }
+        Landmarks landmarks_;
+    }
 
-      //signals to shutdown and joins thread
-      void quit() {
-	   {
- 	     //make sure we have access to the mutex before changing the quit flag
-	     std::lock_guard<std::mutex> lg (data_mutex_);
-             quit_=true;
-	   }
-	   //notify worker thread to wake up and check condition flag
-           data_cv_.notify_all();
-      }
+    virtual ~LandmarkMapper() {}
 
-      //program logic goes in this function
-      void run();
-    private:
-      //should the program exit?
-      bool quit_;
-      //synchronization variables
-      std::mutex data_mutex_;              //mutex variable for image data
-      std::condition_variable data_cv_;    //condition variable associated with images
+    //signals to shutdown and joins thread
+    void quit() {
+        {
+            //make sure we have access to the mutex before changing the quit flag
+            std::lock_guard<std::mutex> lg (data_mutex_);
+                quit_=true;
+        }
+        //notify worker thread to wake up and check condition flag
+        data_cv_.notify_all();
+    }
 
-      std::mutex res_mutex_;              //mutex variable to wait for response on service
-      std::condition_variable res_cv_;    //condition variable associated with service rsponse
-      
-      //image data copies
-      sensor_msgs::msg::Image color_image_;
-      sensor_msgs::msg::PointCloud2 depth_cloud_;
+    //program logic goes in this function
+    void run();
 
-      //synchronized image callback. copies data to local member variables and returns
-      void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr &image, 
-		      const sensor_msgs::msg::PointCloud2::ConstSharedPtr &points);
-      
-      //callback that publishes visualization markers
-      void update_callback();
+private:
+    //should the program exit?
+    bool quit_;
+    //synchronization variables
+    std::mutex data_mutex_;              //mutex variable for image data
+    std::condition_variable data_cv_;    //condition variable associated with images
 
-      //publishers and subscribers
-      message_filters::Subscriber<sensor_msgs::msg::Image> image_subscriber_;
-      message_filters::Subscriber<sensor_msgs::msg::PointCloud2> depth_cloud_subscriber_;
-      //TODO: add Marker Vizualization publisher
-      //rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_publisher_;
-     
-      //synchronization policy 
-      std::shared_ptr<message_filters::TimeSynchronizer<sensor_msgs::msg::Image, sensor_msgs::msg::PointCloud2>> image_sync_;
+    std::mutex res_mutex_;              //mutex variable to wait for response on service
+    std::condition_variable res_cv_;    //condition variable associated with service rsponse
+    
+    //image data copies
+    sensor_msgs::msg::Image color_image_;
+    sensor_msgs::msg::PointCloud2 depth_cloud_;
 
-      //Service client
-      rclcpp::Client<cas726_interfaces::srv::DetectObjects>::SharedPtr detect_client_;
+    //synchronized image callback. copies data to local member variables and returns
+    void image_callback(const sensor_msgs::msg::Image::ConstSharedPtr &image, 
+        const sensor_msgs::msg::PointCloud2::ConstSharedPtr &points);
+    
+    //callback that publishes visualization markers
+    void update_callback();
 
-      rclcpp::TimerBase::SharedPtr timer_;
-      //tf stuff
-      std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
-      std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
-      
-      //parameters
-      std::string image_topic_, depth_cloud_topic_, detect_srv_topic_, odom_frame_, map_frame_, base_frame_; 
+    //publishers and subscribers
+    message_filters::Subscriber<sensor_msgs::msg::Image> image_subscriber_;
+    message_filters::Subscriber<sensor_msgs::msg::PointCloud2> depth_cloud_subscriber_;
+    rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr marker_publisher_;
+    
+    //synchronization policy 
+    std::shared_ptr<message_filters::TimeSynchronizer<sensor_msgs::msg::Image, sensor_msgs::msg::PointCloud2>> image_sync_;
 
-      //Last pose we updated at
-      Eigen::Affine3d last_pose_;
+    //Service client
+    rclcpp::Client<cas726_interfaces::srv::DetectObjects>::SharedPtr detect_client_;
 
-  }; 
+    rclcpp::TimerBase::SharedPtr timer_;
+    //tf stuff
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    
+    //parameters
+    std::string image_topic_, depth_cloud_topic_, detect_srv_topic_, odom_frame_, map_frame_, base_frame_; 
+
+    //Last pose we updated at
+    Eigen::Affine3d last_pose_;
+}; 
 }
 #endif
--- cas726/src/landmark_mapper.cpp
+++ cas726/src/landmark_mapper.cpp
@@ -7,37 +7,45 @@
 
 #include <sensor_msgs/point_cloud2_iterator.hpp>
 
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <pcl/common/common.h>
+#include <pcl/filters/crop_box.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+
 using namespace std::chrono_literals;
 
 //image callback
 void cas726::LandmarkMapper::image_callback(const sensor_msgs::msg::Image::ConstSharedPtr &image, 
-		const sensor_msgs::msg::PointCloud2::ConstSharedPtr &depth_cloud) {
+            const sensor_msgs::msg::PointCloud2::ConstSharedPtr &depth_cloud) {
 
-  std::cerr<<"Got a pair of images\n";
-  //TODO
-  //1. get current odom pose (base link in odom frame) as an Eigen::Affine3d
-  Eigen::Affine3d T; //
-  //2. compute relative transform
-  Eigen::Affine3d movement; //
-  //3. threshold for enough motion
-  double transl = movement.translation().norm();
-  Eigen::AngleAxisd rot(movement.rotation());
-  double angl = rot.angle();
-  //4. if we moved enough, lock the mutex and load up data
-  if(transl > 1 || fabsf(angl) > M_PI/4) {
-    {
-      std::cerr<<"Movement since last pose is "<<transl<<" "<<angl<<std::endl;
-      std::cerr<<"Acquiring lock... ";
-      std::lock_guard<std::mutex> lg(data_mutex_); //acquire lock
-      std::cerr<<"done. Copying images\n";
-      color_image_ = *image;
-      depth_cloud_ = *depth_cloud;
+    std::cerr<<"Got a pair of images\n";
+    //TODO
+    //1. get current odom pose (base link in odom frame) as an Eigen::Affine3d
+    Eigen::Affine3d T; //
+    //2. compute relative transform
+    Eigen::Affine3d movement; //
+    //3. threshold for enough motion
+    double transl = movement.translation().norm();
+    Eigen::AngleAxisd rot(movement.rotation());
+    double angl = rot.angle();
+    //4. if we moved enough, lock the mutex and load up data
+    if(transl > 1 || fabsf(angl) > M_PI/4) {
+        {
+        std::cerr<<"Movement since last pose is "<<transl<<" "<<angl<<std::endl;
+        std::cerr<<"Acquiring lock... ";
+        std::lock_guard<std::mutex> lg(data_mutex_); //acquire lock
+        std::cerr<<"done. Copying images\n";
+        color_image_ = *image;
+        depth_cloud_ = *depth_cloud;
+        }
+        //5. signal worker thread to wake up
+        data_cv_.notify_all();
+        //6. set new last upodate pose
+        last_pose_ = T;
     }
-    //5. signal worker thread to wake up
-    data_cv_.notify_all();
-    //6. set new last upodate pose
-    last_pose_ = T;
-  }
 }
 
 /** This is what the worker thread runs:
@@ -46,94 +54,127 @@ void cas726::LandmarkMapper::image_callback(const sensor_msgs::msg::Image::Const
  *  3. Take detected objects and use them as landmarks
  */
 void cas726::LandmarkMapper::run() {
-  //image data copies
-  sensor_msgs::msg::Image current_color_image;
-  sensor_msgs::msg::PointCloud2 current_depth_cloud;
-  while(true) {
-    {
-      //acquire mutex access
-      std::unique_lock<std::mutex> mutex_locker(data_mutex_); 
-      std::cerr<<"worker thread sleeping\n"; 
-      data_cv_.wait(mutex_locker);     //release lock and wait to be woken up
-      std::cerr<<"worker thread woke up\n"; 
-      //copy data
-      current_color_image = color_image_;
-      current_depth_cloud = depth_cloud_;
-    } //release lock
-    if(!rclcpp::ok()||quit_) break; // check if we are asked to quit
+    //image data copies
+    sensor_msgs::msg::Image current_color_image;
+    sensor_msgs::msg::PointCloud2 current_depth_cloud;
+    while(true) {
+        {
+        //acquire mutex access
+        std::unique_lock<std::mutex> mutex_locker(data_mutex_); 
+        std::cerr<<"worker thread sleeping\n"; 
+        data_cv_.wait(mutex_locker);     //release lock and wait to be woken up
+        std::cerr<<"worker thread woke up\n";
+
+        //copy data
+        current_color_image = color_image_;
+        current_depth_cloud = depth_cloud_;
+        } //release lock
+
+        if(!rclcpp::ok()||quit_) break; // check if we are asked to quit
+        
+        std::cerr<<"worker thread assembling request\n"; 
+        //load up color image in service request
+        auto request = std::make_shared<cas726_interfaces::srv::DetectObjects::Request>();
+        request->color = color_image_;
+
+        //wait for service to exist
+        while (!detect_client_->wait_for_service(1s)) {
+        if (!rclcpp::ok()) {
+            RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+            return;
+        }
+        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting...");
+        }
     
-    std::cerr<<"worker thread assembling request\n"; 
-    //load up color image in service request
-    auto request = std::make_shared<cas726_interfaces::srv::DetectObjects::Request>();
-    request->color = color_image_;
+        //lambda magic to unblock once response is available 
+        auto callback =  [this](rclcpp::Client<cas726_interfaces::srv::DetectObjects>::SharedFutureWithRequest future) { 
+        std::cerr<<"Result callback\n";
+            auto response = future.get();
+        std::cerr<<"Response has "<<response.second->detections.size()<<" objects\n";
+        this->res_cv_.notify_all(); 
+        };
+        auto result = detect_client_->async_send_request(request, std::move(callback));
+        std::cerr<<"Request sent\n";
+        {
+        // Wait for the result.
+        std::unique_lock<std::mutex> response_locker(res_mutex_);
+        std::cerr<<"Waiting for result\n";
+        res_cv_.wait(response_locker);
+        }
 
-    //wait for service to exist
-    while (!detect_client_->wait_for_service(1s)) {
-      if (!rclcpp::ok()) {
-        RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
-        return;
-      }
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting...");
+        std::cerr<<"Resuts received, we have "<<result.get().second->detections.size()<<" objects\n";
+    
+        //TODO: 
+        //clear current landmarks
+        landmarks_.clear();
+
+        //iterate over detections
+        for(auto det : result.get().second->detections) {
+
+        }
+
+        //For each object create a point cloud iterator (sensor_msgs::PointCloud2Iterator)
+        //iterate through point cloud and take out points that are within the bounding box
+
+        sensor_msgs::msg::PointCloud2 transformed_cloud;
+        pcl::PointCloud<pcl::PointXYZI> cloud;
+        pcl::fromROSMsg(depth_cloud_, cloud);
+        
+        //compute average x,y,z
+        //transform point to map frame and save it in the landmark array
+
+        //geometry_msgs::msg::Point 
+        //landmarks_.append("label", )
+
     }
-   
-    //lambda magic to unblock once response is available 
-    auto callback =  [this](rclcpp::Client<cas726_interfaces::srv::DetectObjects>::SharedFutureWithRequest future) { 
-	std::cerr<<"Result callback\n";
-        auto response = future.get();
-	std::cerr<<"Response has "<<response.second->detections.size()<<" objects\n";
-	this->res_cv_.notify_all(); 
-	};
-    auto result = detect_client_->async_send_request(request, std::move(callback));
-    std::cerr<<"Request sent\n";
-    {
-      // Wait for the result.
-      std::unique_lock<std::mutex> response_locker(res_mutex_);
-      std::cerr<<"Waiting for result\n";
-      res_cv_.wait(response_locker);
-    }
-
-    std::cerr<<"Resuts received, we have "<<result.get().second->detections.size()<<" objects\n";
-   
-    //TODO: 
-    //clear current landmarks
-    //iterate over detections
-    //For each object create a point cloud iterator (sensor_msgs::PointCloud2Iterator)
-    //iterate through point cloud and take out points that are within the bounding box
-    //compute average x,y,z
-    //transform point to map frame and save it in the landmark array
-
-  }
 }
 
 //callback that publishes visualization markers
-void cas726::LandmarkMapper::update_callback() {
-  //TODO: publish the current array of landmarks as a visualization marker to displa in rviz
-  std::cout<<"tick tock\n";
+void cas726::LandmarkMapper::update_callback(const std::vector<geometry_msgs::msg::Point>& points) {
+    //TODO: publish the current array of landmarks as a visualization marker to display in rviz
+    visualization_msgs::msg::Marker::SharedPtr shere_list(new visualization_msgs::msg::Marker);
+    shere_list->header.frame_id = map_frame_;
+    shere_list->header.stamp = this->get_clock()->now();
+    shere_list->type = visualization_msgs::msg::Marker::SPHERE_LIST;
+    shere_list->action = visualization_msgs::msg::Marker::ADD;
+    shere_list->points = points;
+    shere_list->scale.x = 0.3; // in meters
+    shere_list->scale.y = 0.3;
+    shere_list->scale.z = 0.3;
+
+    // Set green and alpha(opacity)
+    shere_list->color.g = 1.0;
+    shere_list->color.a = 1.0;
+
+    marker_publisher_.publish(*shere_list);
+    std::cout<<"tick tock\n";
+
+    return;
 }
 
 
 int main(int argc, char ** argv)
 {
-  rclcpp::init(argc,argv);
+    rclcpp::init(argc,argv);
 
-  auto node = std::make_shared<cas726::LandmarkMapper>();
+    auto node = std::make_shared<cas726::LandmarkMapper>();
 
-  //start up a new thread that spins the node
-  std::promise<void> stop_async_spinner;
-  std::thread async_spinner_thread(
-    [stop_token = stop_async_spinner.get_future(), node]() {
-      rclcpp::executors::SingleThreadedExecutor executor;
-      executor.add_node(node);
-      executor.spin_until_future_complete(stop_token);
-      //at this point we are done and should signal the node
-      node->quit();
-    });
+    //start up a new thread that spins the node
+    std::promise<void> stop_async_spinner;
+    std::thread async_spinner_thread(
+      [stop_token = stop_async_spinner.get_future(), node]() {
+        rclcpp::executors::SingleThreadedExecutor executor;
+        executor.add_node(node);
+        executor.spin_until_future_complete(stop_token);
+        //at this point we are done and should signal the node
+        node->quit();
+      });
 
-  //execute program logic
-  node->run();
+    //execute program logic
+    node->run();
 
-  stop_async_spinner.set_value();
-  async_spinner_thread.join();
+    stop_async_spinner.set_value();
+    async_spinner_thread.join();
 
-  return 0;
+    return 0;
 }
